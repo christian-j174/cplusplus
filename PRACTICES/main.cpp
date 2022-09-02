276e359e2198a4c3e03e34949751a73a32f5371b
christian-j174 <christian.rodriguez95@upr.edu>
2022-09-02 01:04:52 -0400

    switch case example

--- PRACTICES/main.cpp
+++ PRACTICES/main.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 
 int main(){
-    cout << "Running tests" << endl;
+    int num;
+    cin >> num;
+    switch(num+2) {
+        case 1:
+            cout << "Case1: Value is: " << num << endl;
+        case 2:
+            cout << "Case2: Value is: " << num << endl;
+        case 3:
+            cout << "Case3: Value is: " << num << endl;
+        default:
+            cout <<"Default: Value is: "<<num<<endl;
+    }
     return 0;
 }
