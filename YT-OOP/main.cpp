412341d9d4ec0805c86cc50c9632aa8d13610466
christian-j174 <christian.rodriguez95@upr.edu>
2022-09-15 13:13:48 -0400

    implemented exmpl abstraction

--- YT-OOP/main.cpp
+++ YT-OOP/main.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 //private, public, protected
-
 //behaviors vs properties
 
-class Employee {
+class AbstractEmployee {        // contract with a pure virtual function
+    virtual void AskForPromotion()=0;
+};
+
+
+class Employee:AbstractEmployee {
     //here we define members of this class
 private:
     string Name;
@@ -25,13 +29,19 @@ public:
     void setAge(int age) {Age = age;}
     int getAge(){ return Age; }
 
-
-
-
+        // METHODS
     void introduceYourSelf() {
         cout << " Name: " << Name << endl;
         cout << " Company: " << Company << endl;
         cout << " Age: " << Age << endl;
+    }
+
+    void AskForPromotion() {
+        if(Age > 30) {
+            cout << "Got Promoted" << endl;
+        } else {
+            cout << ", sorry No promotion for you." << endl;
+        }
     }
     
     // Constructor 
@@ -47,9 +57,10 @@ public:
 
 int main(){ 
     Employee employee1 = Employee("Chris", "ChrisTech", 18);
-    employee1.introduceYourSelf();
-    employee1.setAge(119);
-    cout << employee1.getName() << endl;
-    cout << employee1.getAge() << endl;
+    Employee employee2 = Employee("Idamis", "ChrisTech", 50);
+    cout << employee1.getName();
+    employee1.AskForPromotion();
+    cout << employee2.getName() << endl;
+    employee2.AskForPromotion();
     return 0;
 }
