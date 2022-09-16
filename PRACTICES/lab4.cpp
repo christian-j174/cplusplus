72751ca2fb702f0a0b0526d1f2c0d542cc007be9
christian-j174 <christian.rodriguez95@upr.edu>
2022-09-15 22:23:35 -0400

    percentage method

---
+++ PRACTICES/lab4.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+class Fraction
+{
+private:
+    float numerator;
+    float denominator;
+public:
+    Fraction(float num, float den)
+    {
+        numerator = num;
+        denominator = den;
+    }
+    float getNumerator()
+    {
+        return numerator;
+    }
+    float getDenominator()
+    {
+        return denominator;
+    }
+    /*
+    * Returns the percentage equivalent of the target fraction.
+    * HINT: Check the percentage formula. Use the floor method,
+    * you may review how the floor works by viewing the API.
+    *  Note that the tests expect values that are whole numbers. 
+    */
+    int Fraction::percentage()
+{
+	double result = floor((getNumerator()/getDenominator()) *100);
+	return result;
+}
+    Fraction addInverseOf(Fraction f2);
+    /*
+    * Return a NEW fraction representing the sqrt of the target object.
+    * The resulting fraction may not be expressed in minimal terms.
+    * You must create a NEW fraction object.
+    */
+    Fraction squareRoot();
+    /*
+    * Returns a NEW fraction representing the division of the target fraction
+    * by the argument fraction.  Remember that a/b divided by c/d is equivalent
+    * to a/b times d/c.  The resulting fraction may not be expressed in minimal
+    * terms. You must create a NEW fraction object and able to use getNumerator 
+    * and getDenominator for the tests
+    */
+    Fraction divide(Fraction f2);
+    /*
+    * Return a NEW fraction representing the power of n of the target fraction.
+    * The resulting fraction may not be expressed in minimal terms.
+    * You must create a NEW fraction object.
+    */
+    Fraction powerOf(int n);
+};
