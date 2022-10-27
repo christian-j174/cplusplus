5fd1ad8cc77214f1d667f85495eeb1acf36b4179
christian-j174 <christian.rodriguez95@upr.edu>
2022-10-27 15:28:02 -0400

    done

--- EXAM2/pointers.cpp
+++ EXAM2/pointers.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-/** stores the address of a var in a pointer
- */
+int main() {
+// processes an array using pointer notation 
+    const int SIZE = 5;
+    int numbers[SIZE];
+    int count;
 
+    // Get Values
+    cout << "Enter " << SIZE << " numbers: ";
+    for(count = 0; count < SIZE;count++){
+        cin >> *(numbers + count);
+    }
 
-/* Entonces para definir un pointer
-primero creo una variable y el tipo de dato que va almacenar
-le pongo un * al final.
-
-Si quiero acceder al valor de esa dirreccion en memoria 
-pues cololo el * antes del pointer
-
-y si deseo ver la dirrecion en memoria simplemente 
-pones la variable pointer
-*/
-    int x = 25;
-    int* ptr = nullptr;
-
-    ptr = &x;
-    cout << "The value in x: is: " << x << endl;
-    cout << "The address of x is: " << *ptr << endl;
-    return 0;  
+    //DISPLAY 
+    cout << "NUMBERS THAT YOU ENTERED" <<endl;
+    for(count = 0; count < SIZE;count++)
+        cout << *(numbers + count) << " ";
+    cout << endl;
+    return 0;
 }
+
+
+// int main(){
+// /** stores the address of a var in a pointer
+//  */
+
+
+// /* Entonces para definir un pointer
+// primero creo una variable y el tipo de dato que va almacenar
+// le pongo un * al final.
+
+// Si quiero acceder al valor de esa dirreccion en memoria 
+// pues cololo el * antes del pointer
+
+// y si deseo ver la dirrecion en memoria simplemente 
+// pones la variable pointer
+// */
+//     int x = 25;
+//     int* ptr = nullptr;
+
+//     ptr = &x;
+//     cout << "The value in x: is: " << x << endl;
+//     cout << "The address of x is: " << *ptr << endl;
+//     return 0;  
+// }
